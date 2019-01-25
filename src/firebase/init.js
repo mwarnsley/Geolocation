2f84f6bf91c62ed529d1fb6d88793c684e7166a7
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-01-25 16:15:23 -0500

    Adding in the initial file structure and firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+// Initialize Firebase configuration
+const config = {
+  apiKey: 'AIzaSyA-M4tjwRdQl-skJu4SsJV-_ma4vxKjDkw',
+  authDomain: 'vue-geolocation-f180d.firebaseapp.com',
+  databaseURL: 'https://vue-geolocation-f180d.firebaseio.com',
+  projectId: 'vue-geolocation-f180d',
+  storageBucket: 'vue-geolocation-f180d.appspot.com',
+  messagingSenderId: '823312103733'
+};
+// Initializing the firebase application
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp.firestore();
